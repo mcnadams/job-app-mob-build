c7b306229067af20c8b0b7e2012e9fb267813edf
mcnadams <mcneilbo@oregonstate.edu>
2019-02-05 14:15:40 -0800

    final edits

--- src/applicant-details.js
+++ src/applicant-details.js
@@ -5,14 +5,28 @@ const cityNode = document.getElementById('city');
 const hoursNode = document.getElementById('hours');
 const snoreTypeNode = document.getElementById('snore-types');
 const salaryNode = document.getElementById('salary');
-
-console.log(applicant);
+const messageNode = document.getElementById('message');
 
 nameNode.textContent = applicant.name;
 cityNode.textContent = applicant.city;
 hoursNode.textContent = applicant.hours;
-for(let i = 0; i < applicant.snoreType.length - 1; i++) {
-    snoreTypeNode.textContent += applicant.snoreType[i] + ', ';
+if(applicant.snoreType.length === 0) {
+    snoreTypeNode.textContent = 'none';
 }
-snoreTypeNode.textContent += applicant.snoreType[applicant.snoreType.length - 1];
+else { 
+    for(let i = 0; i < applicant.snoreType.length - 1; i++) {
+        snoreTypeNode.textContent += applicant.snoreType[i] + ', ';
+    }
+    snoreTypeNode.textContent += applicant.snoreType[applicant.snoreType.length - 1];
+}
 salaryNode.textContent = applicant.salary;
+
+if(applicant.hours < 5) {
+    messageNode.textContent = 'You do not sleep enough.';
+}
+else if(applicant.hours > 10) {
+    messageNode.textContent = 'You sleep too much, what\'s wrong with you.';
+}
+else {
+    messageNode.textContent = 'Thank you for your interest, you will be hearing from someone in HR shortly.';
+}
